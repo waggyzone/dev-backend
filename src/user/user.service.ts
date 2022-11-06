5624e3072e71093e696f44c852307f4fc867aef6
sarga <sargasuresh20@gmail.com>
2022-11-06 13:21:55 +0530

    feat: update delete

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,26 @@
 // @ts-check
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { CreateUserDto } from './user.dto';
+import { Model , ObjectId } from 'mongoose';
+import { CreateUserDto ,UpdateUserDto} from './user.dto';
 import { User, UserDocument } from './user.model';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User.name) private userModal:Model<UserDocument>){}
-  
-    async findAllUser() {
+  constructor(@InjectModel(User.name) private userModal: Model<UserDocument>) {}
+
+  async findAllUser() {
     return await this.userModal.find({}).exec();
   }
-  async create(user:CreateUserDto){
-      return await this.userModal.create(user);
+  async create(user: CreateUserDto) {
+    return await this.userModal.create(user);
   }
 
+  async findByIdAndUpdate(id: ObjectId, user: UpdateUserDto) {
+    return await this.userModal.findByIdAndUpdate(id, user);
+  }
+
+  async findByIdAndRemove(id: ObjectId) {
+    return await this.userModal.findByIdAndRemove(id);
+  }
 }
