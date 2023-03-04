bea53d6c3f0cc300e5e4fd40e4200dba5fbf1253
hellosudhiim <hellosudhii@gmail.com>
2023-03-04 23:54:00 +0530

    feat: added swagger api

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as morgan from 'morgan';
 import * as cookieParser from 'cookie-parser';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle("waggy").setVersion('1.0')
+  .build();
+  const document= SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api',app,document)
+
   app.use(morgan('combined'));
   app.enableCors();
   app.use(cookieParser());
